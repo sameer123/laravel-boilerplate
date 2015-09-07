ffa800630bbb5dff5bc80c9434636261e56d5a95
mohin <mlakdawala@aecortech.com>
2015-09-07 13:41:53 +0100

    Changed standard to PSR2

--- setup_sniff.sh
+++ setup_sniff.sh
@@ -3,10 +3,10 @@
 echo -e "Setting up codesniffer..."
 
 # set variables
-PHPCS_BIN=./vendor/bin/phpcs
-INSTALLED_PATHS="./vendor/socialengine/sniffer-rules/src/SocialEngine/SnifferRules/Standard"
+#PHPCS_BIN=./vendor/bin/phpcs
+#INSTALLED_PATHS="$PWD/vendor/socialengine/sniffer-rules/src/SocialEngine/SnifferRules/Standard"
 
-$PHPCS_BIN --config-set installed_paths $INSTALLED_PATHS
+#$PHPCS_BIN --config-set installed_paths $INSTALLED_PATHS
 rm -rf .git/hooks
 ln -s ../contrib/hooks .git/hooks
 
